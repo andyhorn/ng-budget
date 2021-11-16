322fb47d8697da9ecb76dd14ecc038037db8d910
Andy <andyjhorn@gmail.com>
2021-11-15 19:26:43 -0600

    fixed and simplified styling

--- src/app/components/net-flow-icon/net-flow-icon.component.ts
+++ src/app/components/net-flow-icon/net-flow-icon.component.ts
@@ -7,12 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NetFlowIconComponent implements OnInit {
   @Input() netFlow!: number;
-  @Input() dark: boolean = false;
 
   get colorClass(): string {
     return this.netFlow < 0
-      ? this.dark ? 'text-danger' : 'text-danger-light'
-      : this.dark ? 'text-success' : 'text-success-light';
+      ? 'text-danger'
+      : 'text-success';
   }
 
   constructor() { }
