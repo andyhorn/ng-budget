eb064ca0cdad91781e9bed2aba87eb35b10b480f
Andy <andyjhorn@gmail.com>
2021-11-19 23:03:46 -0600

    net flow icon tests

--- src/app/components/net-flow-icon/net-flow-icon.component.spec.ts
+++ src/app/components/net-flow-icon/net-flow-icon.component.spec.ts
@@ -1,4 +1,6 @@
+import { CurrencyPipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NetFlowIconComponent } from './net-flow-icon.component';
 
@@ -21,5 +23,37 @@ describe('NetFlowIconComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return red for net flow less than zero', () => {
+    component.netFlow = -1;
+    fixture.detectChanges();
+
+    expect(component.colorClass).toEqual('text-danger');
+  });
+
+  it('should return green for net flow of zero', () => {
+    component.netFlow = 0;
+    fixture.detectChanges();
+
+    expect(component.colorClass).toEqual('text-success');
+  });
+
+  it('should return green for net flow greater than zero', () => {
+    component.netFlow = 1;
+    fixture.detectChanges();
+
+    expect(component.colorClass).toEqual('text-success');
+  });
+
+  it('should format the net flow as a currency', () => {
+    const netFlow = component.netFlow = 100;
+    fixture.detectChanges();
+
+    const span = fixture.debugElement.query(By.css('div span'));
+    const content = span.nativeElement.innerText;
+    const formatted = new CurrencyPipe('en-US').transform(netFlow);
+
+    expect(content).toEqual(formatted);
   });
 });
