d4a306424212b4e543ec5a1c9f4490e2149caf6f
Andy <andyjhorn@gmail.com>
2021-11-22 09:50:34 -0600

    transaction component tests

--- src/app/components/transaction/transaction.component.spec.ts
+++ src/app/components/transaction/transaction.component.spec.ts
@@ -1,33 +1,125 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { By } from '@angular/platform-browser';
+import { Frequency } from 'src/app/models/recurrence';
+import { Transaction } from 'src/app/models/transaction';
+import { AppStateService } from 'src/app/services/state/app-state.service';
+import { FrequencyDisplayComponent } from '../frequency-display/frequency-display.component';
 
 import { TransactionComponent } from './transaction.component';
 
 describe('TransactionComponent', () => {
+  const id = 1;
+  const amount = 1000;
+  const transactionTitle = 'Test Transaction';
+  const frequency = Frequency.Daily;
+  const interval = 3;
+  const startDate = new Date();
+  const isExpense = true;
   let component: TransactionComponent;
   let fixture: ComponentFixture<TransactionComponent>;
+  let transaction: Transaction;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TransactionComponent ],
+      declarations: [ TransactionComponent, FrequencyDisplayComponent ],
       imports: [
         MatDialogModule,
         MatFormFieldModule,
-        FormsModule
+        FormsModule,
+        MatCardModule,
+      ],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {
+            open: function(component: any, data: any) {}
+          }
+        }
       ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    transaction = new Transaction(transactionTitle, amount, isExpense);
+    transaction.recurrence.frequency = frequency;
+    transaction.recurrence.interval = interval;
+    transaction.recurrence.startDate = startDate;
+    transaction.id = id;
     fixture = TestBed.createComponent(TransactionComponent);
     component = fixture.componentInstance;
+    component.transaction = transaction;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the transaction title', () => {
+    const title = fixture.debugElement.query(By.css('.mat-card-title span'));
+    expect(title).toBeTruthy();
+    const text = title.nativeElement.innerText;
+    expect(text).toEqual(transactionTitle);
+  });
+
+  it('should display the transaction amount, formatted as currency', () => {
+    const currency = new CurrencyPipe('en-US').transform(amount);
+    const subtitle = fixture.debugElement.query(By.css('.mat-card-subtitle span'));
+    expect(subtitle).toBeTruthy();
+    const text = subtitle.nativeElement.innerText;
+    expect(text).toEqual(currency);
+  });
+
+  it('should display the app frequency text', () => {
+    const element = fixture.debugElement.query(By.css('.mat-card-subtitle app-frequency-display'));
+    expect(element).toBeTruthy();
+  });
+
+  it('should display the interval start date', () => {
+    const expected = new DatePipe('en-US').transform(startDate);
+    const subtitleSpans = fixture.debugElement.queryAll(By.css('.mat-card-subtitle p span'));
+    const element = subtitleSpans[subtitleSpans.length - 1];
+    expect(element).toBeTruthy();
+    expect(element.nativeElement.innerText).toContain(expected);
+  });
+
+  it('should remove the transaction from the state when the delete button is clicked', () => {
+    const state = fixture.debugElement.injector.get(AppStateService);
+    const buttons = fixture.debugElement.queryAll(By.css('.mat-card-title button'));
+    const button = buttons.find(e => e.nativeElement.innerText.includes('delete'));
+    let removedId: number;
+    const spy = spyOn(state, 'removeTransaction').and.callFake((id) => removedId = id);
+
+    button?.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(spy.calls.count).toEqual(1);
+    expect(removedId!).toEqual(id);
+  });
+
+  it('should open a dialog with the transaction data when the edit button is clicked', () => {
+    let transactionData: any, dialogComponent: any;
+    const dialog = fixture.debugElement.injector.get(MatDialog);
+    const spy = spyOn(dialog, 'open').and.callFake((component, data) => {
+      dialogComponent = component;
+      transactionData = data;
+      return <MatDialogRef<any, any>>{};
+    });
+
+    const buttons = fixture.debugElement.queryAll(By.css('.mat-card-title button'));
+    const button = buttons.find(e => e.nativeElement.innerText.includes('edit'));
+
+    button?.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(spy.calls.count).toEqual(1);
+    expect(transactionData).toBeTruthy();
+    expect(transactionData.id).toEqual(id);
   });
 });
--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditTransactionDialogData } from 'src/app/models/edit-transaction-dialog-data';
-import { Frequency } from 'src/app/models/recurrence';
 import { Transaction } from 'src/app/models/transaction';
 import { AppStateService } from 'src/app/services/state/app-state.service';
 import { EditTransactionDialogComponent } from '../edit-transaction-dialog/edit-transaction-dialog.component';
@@ -11,17 +10,9 @@ import { EditTransactionDialogComponent } from '../edit-transaction-dialog/edit-
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.sass']
 })
-export class TransactionComponent implements OnInit {
+export class TransactionComponent {
   @Input() transaction!: Transaction;
   @Output() delete: EventEmitter<void>;
-  public amount: number = 0;
-  public frequency!: Frequency;
-  public interval: number = 0;
-  public title: string = '';
-  public startDate!: Date;
-  public isExpense!: boolean;
-  public isExpanded: boolean = false;
-  private _id!: number;
 
   constructor(
     private _state: AppStateService,
@@ -30,26 +21,16 @@ export class TransactionComponent implements OnInit {
     this.delete = new EventEmitter<void>();
   }
 
-  ngOnInit() {
-    this._id = this.transaction.id;
-    this.isExpense = this.transaction.isExpense;
-    this.amount = this.transaction.amount;
-    this.frequency = this.transaction.recurrence.frequency;
-    this.interval = this.transaction.recurrence.interval;
-    this.startDate = this.transaction.recurrence.startDate;
-    this.title = this.transaction.title;
-  }
-
   public onDeleteClick(): void {
     if (confirm('Are you sure you want to delete this transaction?')) {
-      this._state.removeTransaction(this._id);
+      this._state.removeTransaction(this.transaction.id);
     }
   }
 
   public onEditClick(): void {
     this._dialog.open(EditTransactionDialogComponent, {
       width: '480px',
-      data: new EditTransactionDialogData(this._id, false, this.isExpense),
+      data: new EditTransactionDialogData(this.transaction.id, false, this.transaction.isExpense),
     });
   }
 }
