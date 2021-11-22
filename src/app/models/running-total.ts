394b1131d82741f78779eaa42bfb3dc20b4134e1
Andy <andyjhorn@gmail.com>
2021-11-22 13:43:24 -0600

    running total tests

--- src/app/models/running-total.ts
+++ src/app/models/running-total.ts
@@ -2,9 +2,10 @@ import { Occurrence } from "./occurrence";
 
 export class RunningTotal {
   private occurrenceTransactionTotals: number[][] = [];
+  private startingAmount: number = 0;
 
   constructor(occurrences: Occurrence[], startingAmount: number = 0) {
-    let runningTotal: number = startingAmount;
+    let runningTotal: number = this.startingAmount = startingAmount;
 
     for (let oc: number = 0; oc < occurrences.length; oc++) {
       this.occurrenceTransactionTotals.push([]);
@@ -20,6 +21,18 @@ export class RunningTotal {
   }
 
   public getTotal(occurrence: number, transaction: number): number {
+    if (occurrence < 0) {
+      throw 'Invalid argument: occurrence. Must be greater than or equal to zero';
+    }
+
+    if (transaction < 0) {
+      throw 'Invalid argument: transaction. Must be greater than or equal to zero';
+    }
+
+    if (this.occurrenceTransactionTotals.length === 0) {
+      return this.startingAmount;
+    }
+
     if (occurrence >= this.occurrenceTransactionTotals.length) {
       return 0;
     }
