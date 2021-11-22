8f142da02850238c4a485a7d378356928b29937b
Andy <andyjhorn@gmail.com>
2021-11-22 12:45:57 -0600

    removed unused methods from running total

--- src/app/models/running-total.ts
+++ src/app/models/running-total.ts
@@ -1,45 +1,33 @@
 import { Occurrence } from "./occurrence";
 
 export class RunningTotal {
-  private _occurrenceTransactionTotals: number[][] = [];
+  private occurrenceTransactionTotals: number[][] = [];
 
   constructor(occurrences: Occurrence[], startingAmount: number = 0) {
     let runningTotal: number = startingAmount;
 
     for (let oc: number = 0; oc < occurrences.length; oc++) {
-      this._occurrenceTransactionTotals.push([]);
+      this.occurrenceTransactionTotals.push([]);
 
       for (let tr: number = 0; tr < occurrences[oc].transactions.length; tr++) {
         const isExpense: boolean = occurrences[oc].transactions[tr].isExpense;
         const amount: number = occurrences[oc].transactions[tr].amount;
 
         runningTotal = isExpense ? runningTotal - amount : runningTotal + amount;
-        this._occurrenceTransactionTotals[oc].push(runningTotal);
+        this.occurrenceTransactionTotals[oc].push(runningTotal);
       }
     }
   }
 
-  public getNumOccurrences(): number {
-    return this._occurrenceTransactionTotals.length;
-  }
-
-  public getNumTransactions(occurrence: number): number {
-    if (occurrence >= this._occurrenceTransactionTotals.length) {
-      return 0;
-    }
-
-    return this._occurrenceTransactionTotals[occurrence].length;
-  }
-
   public getTotal(occurrence: number, transaction: number): number {
-    if (occurrence >= this._occurrenceTransactionTotals.length) {
+    if (occurrence >= this.occurrenceTransactionTotals.length) {
       return 0;
     }
 
-    if (transaction >= this._occurrenceTransactionTotals[occurrence].length) {
+    if (transaction >= this.occurrenceTransactionTotals[occurrence].length) {
       return 0;
     }
 
-    return this._occurrenceTransactionTotals[occurrence][transaction];
+    return this.occurrenceTransactionTotals[occurrence][transaction];
   }
 }
