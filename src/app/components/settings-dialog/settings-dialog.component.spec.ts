635fa90ad517b00faab100ccf82cb158749f79bd
Andy <andyjhorn@gmail.com>
2021-11-21 22:38:47 -0600

    settings dialog tests

--- src/app/components/settings-dialog/settings-dialog.component.spec.ts
+++ src/app/components/settings-dialog/settings-dialog.component.spec.ts
@@ -1,26 +1,20 @@
-import { DatePipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule, MatDateRangePicker } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatInputHarness } from '@angular/material/input/testing';
 import { By } from '@angular/platform-browser';
-import { HarnessLoader } from '@angular/cdk/testing';
-import { MatDateRangeInputHarness } from '@angular/material/datepicker/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppStateService } from 'src/app/services/state/app-state.service';
 
 import { SettingsDialogComponent } from './settings-dialog.component';
+import { FormsModule } from '@angular/forms';
 
 describe('SettingsDialogComponent', () => {
   let component: SettingsDialogComponent;
   let fixture: ComponentFixture<SettingsDialogComponent>;
-  let loader: HarnessLoader;
-  // let rootLoader: HarnessLoader;
   let matDialogRef: any = {
     close: function () {}
   };
@@ -48,7 +42,6 @@ describe('SettingsDialogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SettingsDialogComponent);
     component = fixture.componentInstance;
-    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
@@ -74,41 +67,14 @@ describe('SettingsDialogComponent', () => {
     expect(state.endDate).toEqual(endDate);
     expect(state.startingAmount).toEqual(startingAmount);
   });
-
-  // it('should display the date range in a picker', async () => {
-  //   const startDate = new Date('January 1, 2020');
-  //   const endDate = new Date('January 15, 2020');
-  //   const expectedStartText = `${startDate.getMonth()}/${startDate.getDate()}/${startDate.getFullYear()}`;
-  //   const expectedEndText = `${endDate.getMonth()}/${endDate.getDate()}/${endDate.getFullYear()}`;
-
-  //   component.startDate = startDate;
-  //   component.endDate = endDate;
-  //   const datePicker = await loader.getHarness(MatDateRangeInputHarness);
-  //   fixture.detectChanges();
-  //   fixture.whenStable().then(async () => {
-  //     const content = await datePicker.getValue();
-  //   });
-
-
-    // const startDateInput = fixture.nativeElement.querySelector('input.mat-start-date');
-    // const endDateInput = fixture.nativeElement.querySelector('input.mat-end-date');
-
-    // const startDateValue = startDateInput.getValue();
-    // const endDateValue = endDateInput.value();
-
-    // expect(startDateInput.nativeElement.value).toEqual(expectedStartText);
-    // expect(endDateInput.nativeElement.value).toEqual(expectedEndText);
-  // });
 });
 
 describe('SettingsDialogComponent', () => {
   let component: SettingsDialogComponent;
   let fixture: ComponentFixture<SettingsDialogComponent>;
-  let loader: HarnessLoader;
   const startDate: Date = new Date('January 1, 2020');
   const endDate: Date = new Date('January 15, 2020');
   const startingAmount: number = 1000;
-  // let rootLoader: HarnessLoader;
   let matDialogRef: any = {
     close: function () {}
   };
@@ -124,6 +90,7 @@ describe('SettingsDialogComponent', () => {
         MatNativeDateModule,
         MatInputModule,
         BrowserAnimationsModule,
+        FormsModule,
       ],
       providers: [
         { provide: MatDialogRef, useValue: matDialogRef },
@@ -140,7 +107,6 @@ describe('SettingsDialogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SettingsDialogComponent);
     component = fixture.componentInstance;
-    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
@@ -149,4 +115,72 @@ describe('SettingsDialogComponent', () => {
     expect(component.endDate).toEqual(endDate);
     expect(component.startingAmount).toEqual(startingAmount);
   });
-})
+
+  it('should set the date range inputs', async () => {
+    const startDateString = toShortDateString(startDate);
+    const endDateString = toShortDateString(endDate);
+    const startDateInput = fixture.debugElement.query(By.css('input.mat-start-date'));
+    const endDateInput = fixture.debugElement.query(By.css('input.mat-end-date'));
+
+    expect(startDateInput.nativeElement.value).toBeFalsy();
+    expect(endDateInput.nativeElement.value).toBeFalsy();
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(startDateInput.nativeElement.value).toEqual(startDateString);
+      expect(endDateInput.nativeElement.value).toEqual(endDateString);
+    });
+  })
+
+  it('should set the starting amount', async () => {
+    const startingAmountInput = fixture.debugElement.query(By.css('input.mat-input-element[type="number"]'));
+
+    expect(startingAmountInput.nativeElement.value).toBeFalsy();
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(startingAmountInput.nativeElement.value).toEqual(startingAmount.toString());
+    })
+  });
+
+  it('should update the state with a new starting amount', async () => {
+    const state = fixture.debugElement.injector.get(AppStateService);
+    const startingAmountInput = fixture.debugElement.query(By.css('input.mat-input-element[type="number"]'));
+
+    startingAmountInput.nativeElement.value = 123;
+    startingAmountInput.nativeElement.dispatchEvent(new Event('Input'));
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      component.onSaveClick();
+      expect(spyOnProperty(state, 'startingAmount').calls.count).toEqual(1);
+      expect(state.startingAmount).toEqual(startingAmount);
+    });
+  });
+
+  it('should update the state with new dates', async () => {
+    const startDateText = toShortDateString(startDate);
+    const endDateText = toShortDateString(endDate);
+    const state = fixture.debugElement.injector.get(AppStateService);
+    const startDateInput = fixture.debugElement.query(By.css('input.mat-start-date'));
+    const endDateInput = fixture.debugElement.query(By.css('input.mat-end-date'));
+
+    startDateInput.nativeElement.value = startDateText;
+    endDateInput.nativeElement.value = endDateText;
+    startDateInput.nativeElement.dispatchEvent(new Event('input'));
+    endDateInput.nativeElement.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      component.onSaveClick();
+      expect(spyOnProperty(state, 'startDate').calls.count).toEqual(1);
+      expect(spyOnProperty(state, 'endDate').calls.count).toEqual(1)
+      expect(state.startDate).toEqual(startDate);
+      expect(state.endDate).toEqual(endDate);
+    });
+  });
+});
+
+function toShortDateString(date: Date): string {
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+}
--- src/app/components/transaction/transaction.component.spec.ts
+++ src/app/components/transaction/transaction.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { TransactionComponent } from './transaction.component';
 
@@ -8,7 +11,12 @@ describe('TransactionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TransactionComponent ]
+      declarations: [ TransactionComponent ],
+      imports: [
+        MatDialogModule,
+        MatFormFieldModule,
+        FormsModule
+      ]
     })
     .compileComponents();
   });
