566dddc5db28e9b0fab738bafe074d1bb3c5a2d6
Andy <andyjhorn@gmail.com>
2021-12-01 09:22:31 -0600

    updated save dialog tests

--- src/app/components/file-save-dialog/file-save-dialog.component.spec.ts
+++ src/app/components/file-save-dialog/file-save-dialog.component.spec.ts
@@ -34,32 +34,37 @@ describe('FileSaveDialogComponent', () => {
   });
 
   it('should return a filename on save', () => {
+    let data: any;
     component.filename = 'Test';
     fixture.detectChanges();
 
     const ref = fixture.debugElement.injector.get(MatDialogRef);
     spyOn(ref, 'close').and.callFake((res) => {
-      expect(res.toString()).toBeTruthy();
+      data = res?.toString();
     });
 
     const button = fixture.debugElement.queryAll(By.css('button'))
       .find(e => e.nativeElement.innerText.includes('Save'));
 
-    expect(button).toBeTruthy();
     button?.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(data).toEqual('Test');
   });
 
   it('should return nothing on cancel', () => {
     const ref = fixture.debugElement.injector.get(MatDialogRef);
+    let data: any = { test: 'there is test data here' };
     spyOn(ref, 'close').and.callFake((res) => {
-      expect(res).toBeFalsy();
+      data = res;
     });
 
     const button = fixture.debugElement.queryAll(By.css('button'))
       .find(e => e.nativeElement.innerText.includes('Cancel'));
 
-    expect(button).toBeTruthy();
     button?.triggerEventHandler('click', {});
+    fixture.detectChanges();
+    expect(data).toBeFalsy();
   });
 
   it('should disable the save button if the field is empty', () => {
@@ -97,5 +102,20 @@ describe('FileSaveDialogComponent', () => {
   it('should contain a text field for the filename', () => {
     const field = fixture.debugElement.query(By.css('input'));
     expect(field).toBeTruthy();
+  });
+
+  it('should trigger a save action when the enter key is pressed', () => {
+    const ref = fixture.debugElement.injector.get(MatDialogRef);
+    let data: any;
+    spyOn(ref, 'close').and.callFake((res) => {
+      data = res;
+    });
+    component.filename = 'test';
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    input.triggerEventHandler('keyup', { code: 'Enter' });
+    fixture.detectChanges();
+    expect(data).toBeTruthy();
   });
 });
